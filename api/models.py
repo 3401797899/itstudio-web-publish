2ed1de2e2476af9106ce662d03b4b93837f8fc69
白 <3401797899@qq.com>
2025-03-19 16:02:02 +0800

    refactor(api): 将`cloudflare_token`替换为`cloudflare_global_api_key`、`cloudflare_email`和`cloudflare_zone_id`

--- api/models.py
+++ api/models.py
@@ -3,7 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 class ConfigurationModel(models.Model):
     cloudflare_tunnel_id = models.CharField(_('CloudFlare Tunnel ID'), max_length=255)
-    cloudflare_token = models.CharField(_('CloudFlare Token'), max_length=255)
+    cloudflare_global_api_key = models.CharField(_('CloudFlare Global API Key'), max_length=255)
+    cloudflare_email = models.CharField(_('CloudFlare Email'), max_length=255)
+    cloudflare_zone_id = models.CharField(_('CloudFlare Zone ID'), max_length=255)
     config_yml_path = models.CharField(_('Config.yml Path'), max_length=255)
     tencent_secret_key = models.CharField(_('Tencent Cloud SecretKey'), max_length=255)
     tencent_secret_id = models.CharField(_('Tencent Cloud SecretID'), max_length=255)
--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,7 @@ from .models import ConfigurationModel
 class ConfigurationSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConfigurationModel
-        fields = ['cloudflare_tunnel_id', 'cloudflare_token', 'config_yml_path',
-                 'tencent_secret_key', 'tencent_secret_id', 'created_at', 'updated_at']
+        fields = ['cloudflare_tunnel_id', 'cloudflare_global_api_key', 'cloudflare_email',
+                 'cloudflare_zone_id', 'config_yml_path', 'tencent_secret_key',
+                 'tencent_secret_id', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
